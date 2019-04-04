75145305210a6bd4b15f924a8e16e23b392f709c
Argens Villarroel <smitterargenis@gmail.com>
2019-04-04 00:39:25 -0400

    Crear Servicio de datos

---
+++ AccountinManager.Data/Services/DataService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccountinManager.Data.Services
+{
+    public abstract class DataService<TEntity, TContext>
+        where TEntity: class
+        where TContext: DbContext
+    {
+        private TContext context;
+        private DbSet<TEntity> table;
+
+        public DataService(TContext context)
+        {
+            this.context = context;
+            table = context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return table;
+        }
+
+
+    }
+}
